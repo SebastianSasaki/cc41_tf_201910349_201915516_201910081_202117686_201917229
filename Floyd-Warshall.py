7e5918620078391e2a1ded1d83fa8cce9fd38ba6
Jorge A Sánchez V <64365546+George2811@users.noreply.github.com>
2021-11-22 00:16:23 -0500

    update algorithm

--- Floyd-Warshall.py
+++ Floyd-Warshall.py
@@ -1,16 +1,25 @@
 import numpy as np
 
-def pseudoFloydWarshall(graph):
+def floydWarshall(G):
+  n = len(G)
 
-  n = len(graph)
-  gain = np.zeros((n, n))
-  for i in range(n):
-    gain[i, i] = 1
-    for j in range(n):
-      gain[i, j] = graph[i, j]
+  cost = [[float('inf')]*n for _ in range(n)]
+  path = [[-1]*n for _ in range(n)]
+
+  for u in range(n):
+    cost[u][u] = 0
+    for v, w in G[u]:
+      cost[u][v] = w
+      path[u][v] = u
+
   for k in range(n):
     for i in range(n):
+      if i == k: continue
       for j in range(n):
-        gain[i, j] = max(gain[i, j], gain[i, k] * gain[k, j])
-        
-  return gain
+        if i == j or j == k: continue
+        f = cost[i][k] + cost[k][j]
+        if f < cost[i][j]:
+          cost[i][j] = f
+          path[i][j] = path[k][j]
+
+  return path, cost
