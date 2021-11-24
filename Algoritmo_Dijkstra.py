8da7bd4bd32c76f560dcabf75f34574daf0e3945
SebastianSasaki <sebastiansasaki@gmail.com>
2021-11-24 09:29:55 -0500

    Update Algoritmo_Dijkstra.py

--- Algoritmo_Dijkstra.py
+++ Algoritmo_Dijkstra.py
@@ -26,3 +26,11 @@ def dijkstra(G, L, s):
           m = path[m]
 
   return min_path, cost
+
+
+# Analisaremos el algoritmo Dijkstra, tambien llamado "Algoritmo de caminos minimos" y veremos su efectividad que tendria en la aplicación del grafo final.
+
+# El Algoritmo de Dijkstra tiene una notación O(V^2) por lo que me da a entender que mientras vertices tenga el grafo más sera su notación. Y que el tiempo de ejecución sera más
+# grande mientras más grande sea el grafo.
+# Vertices: 1000000. Notación Algoritmo Dijkstra: O(1000000^2). Y tenemos que tener en cuenta que se repetira 60 veces, 1 por cada almacen que hay. Todo esto hace que este
+# algoritmo no sea una solución tan óptima.
